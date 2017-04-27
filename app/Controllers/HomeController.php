91783f219caeb873ad6995139ccd3e3c2648a704
Tim McHugh <tim@digitaltim.com>
2017-04-27 15:23:10 -0400

    Add Eloquent database support
    Use $ php -S localhost:8000
    
    Setup to use PostgreSQL
    
    Authentication with Slim 3: Using Eloquent (7/29)
    https://www.youtube.com/watch?v=70IkLMkPyPs&index=7&list=PLfdtiltiRHWGc_yY90XRdq6mRww042aEC

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,8 +7,11 @@ namespace App\Controllers;
 */
 class HomeController extends Controller
 {
-	function index($request, $response)
+	public function index($request, $response)
 	{
+		$user = $this->db->table('users')->find(1);
+		var_dump($user);
+		die();
 		return $this->view->render($response, 'home.twig');
 	}
 }
