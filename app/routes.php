561dfb220d4a44c38a60abc621adaa2e8f41e114
Tim McHugh <tim@digitaltim.com>
2017-04-28 12:29:10 -0400

    Add sign up form
    Use $ php -S localhost:8000
    
    Authentication with Slim 3: Getting ready to sign up (9/29)
    https://www.youtube.com/watch?v=nKu8r_YSQeQ&index=9&list=PLfdtiltiRHWGc_yY90XRdq6mRww042aEC

--- app/routes.php
+++ app/routes.php
@@ -2,3 +2,6 @@
 
 
 $app->get('/', 'HomeController:index');
+
+$app->get('/auth/signup', 'AuthController:getSignUp')->setName('auth.signup');
+$app->post('/auth/signup', 'AuthController:postSignUp');
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -46,5 +46,8 @@ $container['view'] = function ($container) {
 $container['HomeController'] = function ($container) {
 	return new \App\Controllers\HomeController($container);
 };
+$container['AuthController'] = function ($container) {
+	return new \App\Controllers\Auth\AuthController($container);
+};
 
 require __DIR__ . '/../app/routes.php';
