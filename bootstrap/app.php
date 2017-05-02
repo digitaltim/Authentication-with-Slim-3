2fd727b5db9b6b8dd1074d192806be2206152019
Tim McHugh <tim@digitaltim.com>
2017-05-02 12:53:46 -0400

    Add CSRF into middleware
    Use $ php -S localhost:8000
    
    Authentication with Slim 3: View middleware (16/29)
    https://www.youtube.com/watch?v=mHpuCDUPVZQ&list=PLfdtiltiRHWGc_yY90XRdq6mRww042aEC&index=16

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -64,6 +64,7 @@ $container['csrf'] = function ($container) {
 
 $app->add(new \App\Middleware\ValidationErrorsMiddleware($container));
 $app->add(new \App\Middleware\OldInputMiddleware($container));
+$app->add(new \App\Middleware\CsrfViewMiddleware($container));
 
 $app->add($container->csrf);
 
