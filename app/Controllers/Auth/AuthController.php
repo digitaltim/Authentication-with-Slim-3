62935e8a42b95a98eeadc5be3d90aba416b9808e
Tim McHugh <tim@digitaltim.com>
2017-05-02 14:42:27 -0400

    Add sign in form
    Use $ php -S localhost:8000
    
    Authentication with Slim 3: Getting ready to sign in (17/29)
    https://www.youtube.com/watch?v=zBBQuJZEuI4&list=PLfdtiltiRHWGc_yY90XRdq6mRww042aEC&index=17

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -10,6 +10,31 @@ use Respect\Validation\Validator as v;
 */
 class AuthController extends Controller
 {
+	public function getSignIn($request, $response)
+	{
+		return $this->view->render($response, 'auth/signin.twig');
+	}
+
+	public function postSignIn($request, $response)
+	{
+		$validation = $this->validator->validate($request, [
+			'email' => v::noWhitespace()->notEmpty()->email()->emailAvailable(),
+			'name' => v::notEmpty()->alpha(),
+			'password' => v::noWhitespace()->notEmpty(),
+		]);
+
+		if ($validation->failed()) {
+			return $response->withRedirect($this->router->pathFor('auth.signup'));
+		}		
+
+		$user = User::create([
+			'email' => $request->getParam('email'),
+			'name' => $request->getParam('name'),
+			'password' => password_hash($request->getParam('password'), PASSWORD_DEFAULT),
+		]);
+		return $response->withRedirect($this->router->pathFor('home'));
+	}
+
 	public function getSignUp($request, $response)
 	{
 		return $this->view->render($response, 'auth/signup.twig');
