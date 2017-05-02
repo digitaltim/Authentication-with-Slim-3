db2968dc8d61357ccb933259888174f77c59f031
Tim McHugh <tim@digitaltim.com>
2017-05-02 15:43:17 -0400

    Add sign out
    Use $ php -S localhost:8000
    
    Authentication with Slim 3: Signing out (21/29)
    https://www.youtube.com/watch?v=32M6tnv47Y8&index=21&list=PLfdtiltiRHWGc_yY90XRdq6mRww042aEC

--- app/Controllers/Auth/AuthController.php
+++ app/Controllers/Auth/AuthController.php
@@ -10,6 +10,12 @@ use Respect\Validation\Validator as v;
 */
 class AuthController extends Controller
 {
+	public function getSignOut($request, $response)
+    {
+        $this->auth->logout();    
+        return $response->withRedirect($this->router->pathFor('home'));
+    }
+
 	public function getSignIn($request, $response)
 	{
 		return $this->view->render($response, 'auth/signin.twig');
--- app/routes.php
+++ app/routes.php
@@ -8,3 +8,5 @@ $app->post('/auth/signup', 'AuthController:postSignUp');
 
 $app->get('/auth/signin', 'AuthController:getSignIn')->setName('auth.signin');
 $app->post('/auth/signin', 'AuthController:postSignIn');
+
+$app->get('/auth/signout', 'AuthController:getSignOut')->setName('auth.signout');
